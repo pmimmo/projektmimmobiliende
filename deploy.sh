0ccebfe28deae0b839c4adcce7458329e09592c8
Tristan Germer <tgermer@me.com>
2025-09-21 19:27:57 +0200

    Add custom 404 error page and deployment updates
    Introduces a .htaccess file to redirect 404 errors to a new custom 404/index.html page. Updates deploy.sh to upload .htaccess and improve file exclusion during FTP upload. Adds styles for the 404 error page in styles.css.

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,7 @@ npm run build
 # 2. FTP-Upload mit lftp
 lftp -e "
 open -u $FTP_USER,$FTP_PASS $FTP_HOST
-mirror -R --delete dist $FTP_TARGET
+mirror -R --delete --parallel=4 --exclude-glob .DS_Store --exclude-glob '*/.DS_Store' --exclude-glob '._*' dist $FTP_TARGET
+put -O $FTP_TARGET .htaccess
 bye
 "
